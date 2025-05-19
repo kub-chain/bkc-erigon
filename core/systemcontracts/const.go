2115984979876d948ca64fccfd6626e2bf34653a
Nutchanon Chantrasup <beam.nutchanon@bitkub.com>
2025-05-19 18:38:32 +0700

    try fixing system contract upgrade (history v2)

--- core/systemcontracts/const.go
+++ core/systemcontracts/const.go
@@ -19,3 +19,13 @@ var (
 	CrossChainContract         = libcommon.HexToAddress("0x0000000000000000000000000000000000002000")
 	StakingContract            = libcommon.HexToAddress("0x0000000000000000000000000000000000002001")
 )
+
+var (
+	// kub pos contracts
+	KubPosStakeManagerMainnet        = libcommon.HexToAddress("0x443502b3F7C0934576F49CDa084f78640f56A80F")
+	KubPosSlashManagerMainnet        = libcommon.HexToAddress("0xEF6F6c6fdaEAc0326FFE1413D7d7CCAA7B56b753")
+	KubPosStakeManagerStorageMainnet = libcommon.HexToAddress("0xFd98aac1Fbc57e6BC16A167452DBA7af2B6a4c0d")
+	KubPosStakeManagerTestnet        = libcommon.HexToAddress("0xd4a1478020092e624db300714E41b3D39fc6313f")
+	KubPosSlashManagerTestnet        = libcommon.HexToAddress("0x0F717cAC6655D95Fbc6Ee60D06f03bD8ae867e4F")
+	KubPosStakeManagerStorageTestnet = libcommon.HexToAddress("0xC339c3abc4dB5a730C6610058535dBEC52763dD6")
+)
