67bbd2f3f307aab8ed050d37d27d77935aa5aea6
Kongrath 'Joe' Suankaewmanee <joe.kongrath@bitkub.com>
2025-06-09 15:30:59 +0700

    Edit version.go

--- params/version.go
+++ params/version.go
@@ -33,7 +33,7 @@ var (
 const (
 	VersionMajor       = 1       // Major version component of the current release
 	VersionMinor       = 1       // Minor version component of the current release
-	VersionMicro       = 0       // Patch version component of the current release
+	VersionMicro       = 1       // Patch version component of the current release
 	VersionModifier    = "beta1" // Modifier component of the current release
 	VersionKeyCreated  = "ErigonVersionCreated"
 	VersionKeyFinished = "ErigonVersionFinished"
